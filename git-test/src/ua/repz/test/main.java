e5237331ee8bfee88a05a2302c4a888a7926f9f6
User <User@win-100>
2019-04-12 12:18:51 +0300

    add !!

--- git-test/src/ua/repz/test/main.java
+++ git-test/src/ua/repz/test/main.java
@@ -3,7 +3,7 @@ package ua.repz.test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Привет из github");
+		System.out.println("Привет из github!!!!");
 	}
 
 }
